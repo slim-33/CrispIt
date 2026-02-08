52020195a085e7be459ff35f2c6ad5784da47158
kuan <rueikuanwei@gmail.com>
2026-02-08 07:58:44 -0800

    revert to render url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,1 @@
-// TODO: revert to 'https://crispit-server.onrender.com' before deploying
-export const API_URL = 'http://172.21.86.87:3000';
+export const API_URL = 'https://crispit-server.onrender.com';
